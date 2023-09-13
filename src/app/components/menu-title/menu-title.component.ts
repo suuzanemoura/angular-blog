23845e7ed1b5509162c3b44452f6ac233705dcbb
Suzane Moura <suuzanemoura@gmail.com>
2023-09-13 20:28:12 -0300

    :spakles: feat: header com navegação e título feitos

---
+++ src/app/components/menu-title/menu-title.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-title',
+  templateUrl: './menu-title.component.html',
+  styleUrls: ['./menu-title.component.css']
+})
+export class MenuTitleComponent {
+  @Input() title:string = ''
+}
