75e6d0e99f5f8c06983d99ac1df9af5c25b33240
Suzane Moura <suuzanemoura@gmail.com>
2023-09-14 21:53:59 -0300

    :sparkles: feat: seção de web deveplopment criado

---
+++ src/app/components/medium-card/medium-card.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-medium-card',
+  templateUrl: './medium-card.component.html',
+  styleUrls: ['./medium-card.component.css']
+})
+export class MediumCardComponent {
+  @Input() border:string = ''
+}
