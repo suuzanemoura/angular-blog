65ec351069770193d6728db0bf5b0a507c585cf9
Suzane Moura <suuzanemoura@gmail.com>
2023-09-19 11:35:55 -0300

    :bug: bugfix: routerlink mobile no menu nav com id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { DesignComponent } from './components/design/design.component';
+import { WebDevComponent } from './components/web-dev/web-dev.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   {
     path:'',
+    component:HomeComponent
+  },
+  {
+    path:'blog',
+    component:HomeComponent
+  },
+  {
+    path:'design',
+    component:HomeComponent
+  },
+  {
+    path:'webdevelopment',
+    component:HomeComponent
+  },
+  {
+    path:'about',
     component:HomeComponent
   },
   {
